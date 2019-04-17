89c0307c95872b721010611cd7d1f248de75a50c
William Tomé <william.tomenh@gmail.com>
2019-04-17 16:54:28 -0300

    Adicionados métodos interceptadores  getter e setter

--- bootstrap.php
+++ bootstrap.php
@@ -2,20 +2,24 @@
 
 require __DIR__.'/autoload.php';
 
-// $person = new WilliamTome\People\Person;
-// $person->setName("William");
-// $person->setAge(30);
-// $person->setWeight(65);
+$person = new WilliamTome\People\Person;
+$person->name = 'Eric';
+$person->age = 30;
+$person->weight = 65;
 
-// var_dump($person);
+var_dump($person->name);
+var_dump($person->age);
+
+echo $person;
 
 // var_dump((new WilliamTome\DB\MySQL)->connect());
 // var_dump((new WilliamTome\DB\Postgres)->connect());
 
 // var_dump((new WilliamTome\DB\ORM)->select(false));
 
-try {
-    (new WilliamTome\DB\ORM)->select(false);
-} catch (WilliamTome\MyException $e) {
-    echo "Erro: ".$e->getMessage();
-}
+// try {
+//     (new WilliamTome\DB\ORM)->select(false);
+// } catch (WilliamTome\MyException $e) {
+//     echo "Erro: ".$e->getMessage();
+// }
+
--- src/People/Person.php
+++ src/People/Person.php
@@ -4,15 +4,43 @@ namespace WilliamTome\People;
 
 class Person{
 
-    private $data = [];
+    private $data;
+    private $age;
+    private $weight;
+
+    public function __construct() {
+        echo 'classe instanciada' . PHP_EOL;
+    }
 
     public function setName(string $name){
         $this->data[] = $name;
     }
     public function setAge(int $age){
-        $this->data[] = $age;
+        $this->age = $age;
     }
     public function setWeight(float $weight){
-        $this->data[] = $weight;
+        $this->weight = $weight;
+    }
+
+    public function __set($propertie, $value)
+    {
+        $method = 'set' . ucfirst($propertie);
+        if (method_exists($this, $method)) {
+            $this->$method($value);
+        }
+        $this->propertie = $value;
+    }
+    
+    public function __get($propertie)
+    {
+        $method = 'get' . ucfirst($propertie);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+        return $this->propertie;
+    }
+
+    public function __toString() :string{
+        return $this->name . ', ' . $this->age . ', ' . $this->weight . '.';
     }
 }
